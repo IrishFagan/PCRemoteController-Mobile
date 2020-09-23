39d823f4967eaef23fa6b2a2e92607959029f29e
Irish Fagan <IrishFagan@live.com>
2020-09-23 13:06:04 -0700

    Revise styling

--- src/components/MouseInput.js
+++ src/components/MouseInput.js
@@ -4,9 +4,27 @@ import io from 'socket.io-client'
 
 const MouseInput = () => {
   return(
-    <View>
+    <View style={[styles.input, styles.blackBorder]}>
+      <View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    width: '90%',
+    height: '40%',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    margin: '5%',
+    backgroundColor: 'grey'
+  },
+  blackBorder: {
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 7
+  }
+})
 
 export default MouseInput
