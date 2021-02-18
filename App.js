dba7ffb91f8ce0625a4362f369d2e6152f990c05
IrishFagan <irishfagan@live.com>
2021-02-17 22:34:02 -0800

    Pull out websocket initialization into App.js

--- App.js
+++ App.js
@@ -2,11 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MouseInput from './src/components/MouseInput'
+import ButtonInput from './src/components/ButtonInput'
 
 export default function App() {
+
+  const socket = new WebSocket('ws://192.168.0.28:8080')
+
+  socket.addEventListener('open', () => console.log('Connected!!'))
+  socket.addEventListener('error', (err) => console.log(err))
+
   return (
     <View style={styles.container}>
-      <MouseInput />
+      <MouseInput
+        socket={socket}
+      />
+      <ButtonInput
+        socket={socket}
+        name="Left Click"
+        evt="LEFT_CLICK"
+      />
     </View>
   );
 }
